719219a8d4d0df9414b9c6b394ad36198eba08b4
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-05-31 13:15:07 +0800

    ℹ️ Add company & device info

--- lib/pages/app_about.dart
+++ lib/pages/app_about.dart
@@ -1,5 +1,8 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+
+import '../util/link_launcher.dart';
 
 class AppAbout extends StatefulWidget {
   const AppAbout({super.key});
@@ -9,10 +12,24 @@ class AppAbout extends StatefulWidget {
 }
 
 class _AppAboutState extends State<AppAbout> {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        ListTile(
+          leading: const CircleAvatar(
+            backgroundColor: Colors.green,
+            child: Icon(Icons.info, color: Colors.white),
+          ),
+          title: const Text('Israk Solutions Sdn. Bhd.'),
+          subtitle: const Text('https://www.israk.my'),
+          onTap: () {
+            LinkLauncher.launch('https://www.israk.my/');
+          },
+        ),
+        const Divider(),
         ListTile(
           leading: const CircleAvatar(
             backgroundColor: Colors.purple,
@@ -23,6 +40,20 @@ class _AppAboutState extends State<AppAbout> {
               future: PackageInfo.fromPlatform(),
               builder: (_, snapshot) {
                 return Text(snapshot.data?.version ?? 'N/A');
+              }),
+        ),
+        ListTile(
+          leading: const CircleAvatar(
+            backgroundColor: Colors.grey,
+            child: Icon(Icons.phone_android, color: Colors.white),
+          ),
+          title: const Text('Device Info'),
+          subtitle: FutureBuilder(
+              future: deviceInfo.androidInfo,
+              builder: (_, snapshot) {
+                var androidVersion = snapshot.data?.version.release ?? 'N/A';
+                var androidSdk = snapshot.data?.version.sdkInt ?? 'Sdk?';
+                return Text('Android $androidVersion SDK $androidSdk');
               }),
         ),
       ],
