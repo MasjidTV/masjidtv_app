4e67452fe2d603654c5b532ab9f0bdbbe2325aa2
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-06-02 07:16:47 +0800

    :alembic: Test implementation from https://stackoverflow.com/questions/70795926/flutter-app-boot-completed-receiver-doesnt-work
    It works (the app successfully launches after boot). But..
    It may not support Android <M

--- android/app/src/main/kotlin/my/israk/masjidtv_app/MainActivity.kt
+++ android/app/src/main/kotlin/my/israk/masjidtv_app/MainActivity.kt
@@ -1,6 +1,27 @@
 package my.israk.masjidtv_app
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.Settings
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // https://stackoverflow.com/questions/70795926/flutter-app-boot-completed-receiver-doesnt-work
+        var REQUEST_OVERLAY_PERMISSIONS = 100
+        if (!Settings.canDrawOverlays(getApplicationContext())) {
+            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            val uri: Uri = Uri.fromParts("package", getPackageName(), null)
+            myIntent.setData(uri)
+            startActivityForResult(myIntent, REQUEST_OVERLAY_PERMISSIONS)
+            return
+        }
+    }
+
 }
