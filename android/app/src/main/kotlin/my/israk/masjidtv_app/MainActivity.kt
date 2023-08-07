779c3ffc5dbad2d4a19a6323ba9a4099e8bdeea9
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-08-08 04:35:58 +0800

    :bug: Fixed crash when app start on android 5-

--- android/app/src/main/kotlin/my/israk/masjidtv_app/MainActivity.kt
+++ android/app/src/main/kotlin/my/israk/masjidtv_app/MainActivity.kt
@@ -11,21 +11,22 @@ import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // https://stackoverflow.com/questions/70795926/flutter-app-boot-completed-receiver-doesnt-work
-        var REQUEST_OVERLAY_PERMISSIONS = 100
         // Overlay permission is only required for Marshmallow (API 23) and above.
         // In previous APIs this permission is provided by default.
-        if (!Settings.canDrawOverlays(getApplicationContext())) {
-            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            val uri: Uri = Uri.fromParts("package", getPackageName(), null)
-            myIntent.setData(uri)
-            startActivityForResult(myIntent, REQUEST_OVERLAY_PERMISSIONS)
-            Toast.makeText(getApplicationContext(),"App started",Toast.LENGTH_SHORT).show();
-            return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val REQUEST_OVERLAY_PERMISSIONS = 100
+            if (!Settings.canDrawOverlays(getApplicationContext())) {
+                val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                val uri: Uri = Uri.fromParts("package", getPackageName(), null)
+                myIntent.setData(uri)
+                startActivityForResult(myIntent, REQUEST_OVERLAY_PERMISSIONS)
+            }
         }
+        Toast.makeText(getApplicationContext(),"App started",Toast.LENGTH_SHORT).show();
+        return
     }
 
 }
